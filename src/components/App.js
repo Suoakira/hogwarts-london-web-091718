67ddbabfa0c54488ff726d23424b7f5a731e7dc5
github username <github email address>
2018-11-23 10:09:48 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,9 @@ class App extends Component {
     super()
     this.state = {
       hogs: [],
-      hogsFilterClicked: false
+      hogsFilterClicked: true,
+      hogsFilterWeight: true,
+      hogsFilterName: true
     }
   }
 
@@ -26,24 +28,62 @@ class App extends Component {
   }
 
   filterGreasedHogs = () => {
-    this.toggleClicked()
-    const newHogs = this.state.hogs.filter(hog => hog.greased === true)
-
-    this.state.hogsFilterClicked ? this.setState({ hogs: newHogs }) : this.setState({ hogs: hogs })
-    console.log(newHogs)
-    
+    this.toggleClicked() // toggles hogsFilterClicked
+    const newHogs = this.state.hogs.filter(hog => hog.greased === true) // gets greased Hogs
+    this.state.hogsFilterClicked ? this.setState({ hogs: newHogs }) : this.setState({ hogs: hogs }) 
   }
+  
+  // filter by weight
+  filterHogsByWeight = () => {
+    this.setState({
+      hogsFilterWeight: !this.state.hogsFilterWeight
+    })
+
+    this.state.hogsFilterWeight ? 
+      this.setState({ hogs: this.state.hogs.sort((a, b) => b['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'] - a['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']) }) 
+    :
+      this.setState({ hogs: this.state.hogs.sort((a, b) => a['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'] - b['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']) }) 
+  }
+
+  // filter by name
+  sortByName = () =>  {
+    this.setState({
+      hogsFilterName: !this.state.hogsFilterName
+    })
+    console.log(this.state.hogsFilterName)
+
+    this.state.hogsFilterName ? 
+    // filter by name
+      this.setState({ hogs: this.state.hogs.sort((a, b) => a.name.localeCompare(b.name) )})
+    : 
+      this.setState({ hogs: this.state.hogs.sort((a, b) => b.name.localeCompare(a.name) )})
+
+  }
+
+
+
+
+
+
   
   render() {
     const { hogs } = this.state
+    const { filterGreasedHogs, filterHogsByWeight, sortByName } = this
 
     return (
       <div className="App">
-        < Nav />
+        < Nav filterGreasedHogs={ filterGreasedHogs } thisString={"hello"} />
         <button className="ui secondary button" onClick={() => this.filterGreasedHogs()}>
+          Displaying { this.state.hogsFilterClicked ? "Greased" : "Not-Greased" }
+        </button>
 
-          Filter Greased {this.state.hogsFilterClicked ? "Greased" : "Not Greased" }
-          </button>
+        <button onClick={ () => filterHogsByWeight() } className="ui secondary button">
+          Filter By Weight
+        </button>
+
+        <button onClick={() => sortByName() } className="ui secondary button">
+          Filter By Name
+        </button>
 
         < HogIndex hogs={ hogs }  />
 
--- src/components/Hogtile.js
+++ src/components/Hogtile.js
@@ -17,7 +17,7 @@ export default class Hogstile extends Component {
         return this.state.clicked ?  
             (
             <div className="ui eight wide column">
-                    <div onClick={() => this.setState({ clicked: false })} className="ui card">
+                    <div onClick={ () => this.setState({ clicked: false }) } className="ui card">
                     <div class="image">
                         <img src="https://via.placeholder.com/150" />
                     </div>
@@ -30,16 +30,16 @@ export default class Hogstile extends Component {
                 :
                 (
             <div className="ui eight wide column">
-                    <div onClick={() => this.setState({ clicked: true })} className="ui card">
+                    <div onClick={ () => this.setState({ clicked: true }) } className="ui card">
                     <div class="image">
                         <img src="https://via.placeholder.com/150" />
                     </div>
                     <div className="content">
-                        <p>Name: <a className="header">{hog.name}</a></p>
-                        <p>specialty: <a className="header">{hog.specialty}</a></p>
+                        <p>Name: <a className="header">{ hog.name }</a></p>
+                        <p>specialty: <a className="header">{ hog.specialty }</a></p>
                         <p>Greased: <a className="header">{ hog.greased ? "Greased" : "Not-Greased" }</a></p>
-                        <p>Best Medal: <a className="header">{hog["highest medal achieved"]}</a></p>
-                            <p>Weight Ratio: <a className="header">{hog["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"]}</a></p>
+                        <p>Best Medal: <a className="header">{ hog["highest medal achieved"] }</a></p>
+                            <p>Weight Ratio: <a className="header">{ hog["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"] }</a></p>
                     </div>
                 </div>
             </div>
